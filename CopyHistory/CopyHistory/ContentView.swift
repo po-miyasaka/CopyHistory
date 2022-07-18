260f4129557ddba8af1852c4fb2e5c21855f471c
miyashi <miyasaka@pepabo.com>
2022-07-19 03:37:07 +0900

    keyboard short cut

--- CopyHistory/CopyHistory/ContentView.swift
+++ CopyHistory/CopyHistory/ContentView.swift
@@ -11,44 +11,78 @@ struct ContentView: View {
     @StateObject var pasteboardService: PasteboardService = .build()
     @FocusState var isFocus
     @State var isAlertPresented: Bool = false
+    @State var focusedItemIndex: Int?
     var body: some View {
         Group {
-            TextField("search (⌘ + f)", text: $pasteboardService.searchText)
-                .padding(.horizontal)
-                .padding(.top, 16)
-                .padding(.bottom, 8)
-                .focused($isFocus)
-                .textFieldStyle(.roundedBorder)
-                .onChange(of: pasteboardService.searchText, perform: { _ in
-                    withAnimation {
-                        pasteboardService.search()
-                    }
-                })
-                .foregroundColor(.primary)
+            HStack(alignment: .center, spacing: 10) {
+                TextField("search (⌘ + f)", text: $pasteboardService.searchText)
 
-            HStack {
+                    .focused($isFocus)
+                    .textFieldStyle(.roundedBorder)
+                    .onChange(of: pasteboardService.searchText, perform: { _ in
+                        focusedItemIndex = nil
+                        withAnimation {
+                            pasteboardService.search()
+                        }
+                    })
+                    .foregroundColor(.primary)
                 Text("\(pasteboardService.copiedItems.count)")
                     .font(.caption)
                     .foregroundColor(Color.gray)
+            }.padding()
+
+            HStack(alignment: .bottom) {
+                VStack(alignment: .trailing) {
+                    Text("up: ⌘ + ↑ ")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 1)
+
+                    Text("down: ⌘ + ↓ ")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 1)
+
+                    Text("select: ⌘ + ↩")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 1)
+
+                }
+
                 Spacer()
 
-                Button(action: {
-                    withAnimation {
-                        pasteboardService.favoriteFilterButtonDidTap()
-                    }
+                VStack(spacing:0) {
+                    Button(action: {
+                        withAnimation {
+                            pasteboardService.favoriteFilterButtonDidTap()
+                        }
 
-                }, label: {
-                    Image(systemName: pasteboardService.isShowingOnlyFavorite ? "star.fill" : "star")
-                        .foregroundColor(pasteboardService.isShowingOnlyFavorite ? Color.mainAccent : Color.primary)
-                })
-                .keyboardShortcut("s", modifiers: .command)
+                    }, label: {
+
+                        Image(systemName: pasteboardService.isShowingOnlyFavorite ? "star.fill" : "star")
+                            .foregroundColor(pasteboardService.isShowingOnlyFavorite ? Color.mainAccent : Color.primary)
+                    })
+                    .keyboardShortcut("s", modifiers: .command)
+
+                    Text("⌘ + s").font(.caption).foregroundColor(.secondary).padding(.top, 2)
+                }
+
+
             }
             .padding(.horizontal)
-            List(pasteboardService.copiedItems) { item in
+            List(Array(zip(pasteboardService.copiedItems.indices, pasteboardService.copiedItems)), id: \.1.dataHash) { index, item in
                 Row(item: item,
-                    didSelected: { item in pasteboardService.didSelected(item) },
+                    didSelected: { item in
+                    focusedItemIndex = nil
+                    pasteboardService.didSelected(item)
+                },
                     favoriteButtonDidTap: { item in pasteboardService.favoriteButtonDidTap(item) },
-                    deleteButtonDidTap: { item in pasteboardService.deleteButtonDidTap(item) })
+                    deleteButtonDidTap: { item in pasteboardService.deleteButtonDidTap(item) },
+                    isFocused: index == focusedItemIndex
+                )
+
+
             }
             .border(.separator, width: 1.0)
             .listStyle(.inset(alternatesRowBackgrounds: false))
@@ -56,11 +90,7 @@ struct ContentView: View {
                 isFocus = true
             }
             HStack {
-                Button(action: {
-                    isFocus = true
-                }, label: {})
-                .opacity(.leastNonzeroMagnitude)
-                .keyboardShortcut("f", modifiers: .command)
+                KeyboardShortcutCommands()
                 Spacer()
 
                 Button(action: {
@@ -99,6 +129,65 @@ struct ContentView: View {
             }
         )
     }
+
+    func selectDown() {
+        if let i = focusedItemIndex, pasteboardService.copiedItems.count > i {
+            focusedItemIndex = i + 1
+        } else {
+            focusedItemIndex = 0
+        }
+    }
+
+    func selectUp() {
+        if let i = focusedItemIndex, i >= 0 {
+            focusedItemIndex = i - 1
+        } else {
+            focusedItemIndex = pasteboardService.copiedItems.endIndex
+        }
+    }
+    @ViewBuilder
+    func KeyboardShortcutCommands() -> some View {
+        Group{
+            Button(action: {
+                isFocus = true
+            }, label: {})
+            .opacity(.leastNonzeroMagnitude)
+            .keyboardShortcut("f", modifiers: .command)
+
+            Button(action: {
+                selectDown()
+            }, label: {})
+            .opacity(.leastNonzeroMagnitude)
+            .keyboardShortcut(.downArrow, modifiers: .command)
+
+            Button(action: {
+                selectDown()
+            }, label: {})
+            .opacity(.leastNonzeroMagnitude)
+            .keyboardShortcut("j", modifiers: .command)
+
+            Button(action: {
+                selectUp()
+            }, label: {})
+            .opacity(.leastNonzeroMagnitude)
+            .keyboardShortcut(.upArrow, modifiers: .command)
+
+            Button(action: {
+                selectUp()
+            }, label: {})
+            .opacity(.leastNonzeroMagnitude)
+            .keyboardShortcut("k", modifiers: .command)
+
+            Button(action: {
+                if let i = focusedItemIndex, pasteboardService.copiedItems.endIndex > i {
+                    pasteboardService.didSelected(pasteboardService.copiedItems[i])
+                    focusedItemIndex = nil
+                }
+            }, label: {})
+            .opacity(.leastNonzeroMagnitude)
+            .keyboardShortcut(.return, modifiers: .command)
+        }
+    }
 }
 
 struct Row: View {
@@ -106,7 +195,7 @@ struct Row: View {
     let didSelected: (CopiedItem) -> Void
     let favoriteButtonDidTap: (CopiedItem) -> Void
     let deleteButtonDidTap: (CopiedItem) -> Void
-
+    let isFocused: Bool
     var body: some View {
         VStack {
             HStack {
@@ -119,7 +208,8 @@ struct Row: View {
                     VStack {
                         HStack {
                             Text(item.name ?? "No Name")
-                                .font(.body)
+                                .font(.callout)
+                                .foregroundColor(isFocused ? .mainAccent : .primary)
                             Spacer()
                         }
                     }
