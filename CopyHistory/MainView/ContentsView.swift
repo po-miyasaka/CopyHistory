ffa293976d194149a2cb07e9a1525df8700bfd7c
po-miyasaka <miyasaka_kazutoshi@yahoo.co.jp>
2023-09-16 02:01:44 +0900

    lint

--- CopyHistory/MainView/ContentsView.swift
+++ CopyHistory/MainView/ContentsView.swift
@@ -18,9 +18,9 @@ extension MainView {
                     // ・This doesn't make ScrollView + ForEach make additional padding, https://www.reddit.com/r/SwiftUI/comments/e607z3/swiftui_scrollview_foreach_padding_weird/
                     // ・Lazy improves the performance of the inclement search.
                     // However Lazy make selecting cell work weird...
-                    
+
                     ForEach(Array(zip(viewModel.copiedItems.indices, viewModel.copiedItems)), id: \.1.dataHash) { index, item in
-                        
+
                         Row(item: item,
                             favorite: item.favorite,
                             isFocused: index == focusedItemIndex, itemAction: $itemAction,
@@ -38,19 +38,19 @@ extension MainView {
                 }
                 KeyboardCommandButtons(action: { scroll(proxy: proxy, direction: .down) }, keys:
                                         [.init(main: .downArrow, sub: .command), .init(main: "j", sub: .command)])
-                
+
                 KeyboardCommandButtons(action: { scroll(proxy: proxy, direction: .up) }, keys:
                                         [.init(main: .upArrow, sub: .command), .init(main: "k", sub: .command)])
                 Shortcuts()
             }
         }
     }
-    
+
     enum Direction {
         case up
         case down
     }
-    
+
     func scroll(proxy: ScrollViewProxy, direction: Direction) {
         if viewModel.copiedItems.isEmpty {
             return
@@ -63,7 +63,7 @@ extension MainView {
             } else {
                 _focusedItemIndex = 0
             }
-            
+
         case .up:
             if let i = focusedItemIndex, i > 0 {
                 _focusedItemIndex = i - 1
@@ -102,7 +102,7 @@ struct Row: View, Equatable {
         _isShowingHTML = isShowingHTML
         memo = item.memo ?? ""
     }
-    
+
     var body: some View {
         VStack {
             HStack {
@@ -115,16 +115,14 @@ struct Row: View, Equatable {
                     //                        keys: [.init(main: "i", sub: .command)]
                     //                    )
                 }
-                
+
                 Button(action: {
                     itemAction = .init(item: item, action: .select)
                 }, label: {
-                    
-                    
-                    
+
                     ZStack {
                         Color.mainViewBackground.opacity(0.1)
-                        
+
                         // spreading Button's Tap area was very difficult , but ZStack + Color make it but .clear is not available
                         // TODO: survey for alternative to Color
                         //
@@ -138,20 +136,20 @@ struct Row: View, Equatable {
                                         Image(nsImage: image).resizable().scaledToFit().frame(maxHeight: 300)
                                     } else if isShowingRTF, let attributedString = item.attributeString {
                                         Text(AttributedString(attributedString))
-                                        
+
                                     } else if isShowingHTML, let attributedString = item.htmlString {
                                         Text(AttributedString(attributedString))
                                     } else if let url = item.fileURL {
                                         // TODO:
                                         // I want to show images from fileURL,
                                         // but images disappear next time when it's shown
-                                        
+
                                         FileImageView(url: url)
                                     } else {
                                         Text(item.name ?? "No Name").font(.callout)
                                     }
                                 }.padding(.vertical, memo.isEmpty ? 8 : 4).lineLimit(isExpanded ? 20 : 1)
-                                
+
                                 Spacer()
                             }
                             if !memo.isEmpty {
@@ -164,18 +162,18 @@ struct Row: View, Equatable {
                         }
                     }
                 })
-                
+
                 VStack(alignment: .trailing) {
                     Text(item.contentTypeString ?? "").font(.caption)
                     Text("\(item.binarySizeString)").font(.caption)
                 }
-                
+
                 TextField("", text: $memo)
                     .focused($memoFocusState)
                     .onSubmit({
                         itemAction = .init(item: item, action: .memoEdited(memo))
                     }).frame(width: 26)
-                
+
                 Button(action: {
                     itemAction = .init(item: item, action: .favorite)
                 }, label: {
@@ -184,19 +182,19 @@ struct Row: View, Equatable {
                         .frame(width: 30, height: 44)
                         .contentShape(RoundedRectangle(cornerRadius: 20))
                 })
-                
+
                 Button(action: {
                     itemAction = .init(item: item, action: .delete)
                 }, label: {
                     Image(systemName: "trash.fill").foregroundColor(.secondary)
                 })
-                
+
             }
         }
         .buttonStyle(PlainButtonStyle())
         Divider()
     }
-    
+
     /// This comparation make Row stop unneeded rendering.
     static func == (lhs: Row, rhs: Row) -> Bool {
         return lhs.item.dataHash == rhs.item.dataHash &&
@@ -208,7 +206,7 @@ struct Row: View, Equatable {
 struct FileImageView: View {
     let url: URL
     @State var image: NSImage?
-    
+
     var body: some View {
         Group {
             if let image {
@@ -226,16 +224,16 @@ struct FileImageView: View {
                 //
                 //                    ///This method should not be called on the main thread as it may lead to UI unresponsiveness.
                 //                }
-                
+
                 /// 結局Xcodeのバグっぽい。
                 /// https://ios-docs.dev/this-method-should/
-                
+
                 //                image = await loadImage(from: url)
             }
-            
+
         }
     }
-    
+
     func loadImage(from url: URL) async -> NSImage? {
         return NSImage(contentsOf: url)
     }
@@ -248,7 +246,7 @@ struct KeyboardCommandButtons: View {
         let main: KeyEquivalent
         let sub: EventModifiers
     }
-    
+
     let action: () -> Void
     let keys: [Key]
     var body: some View {
